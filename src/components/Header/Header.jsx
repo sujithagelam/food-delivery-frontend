36f1d645e427edda5eda79fbc1caa6441a542476
suji-gelam <sujithagelam@gmail.com>
2026-07-25 11:25:28 +0530

    Initial frontend commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favorite food here</h2>
+        <p>
+          Choose from a diverse menu featuring a delectable array of dishes
+          crated with the finest ingredents and culinary expertise.Our mission
+          is to satisfy your cravings and eleveate your dinign exxperisnec ,One
+          delicion meal at a time
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
