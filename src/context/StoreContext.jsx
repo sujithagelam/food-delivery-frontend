9a5625a9981de8015c7b0e96b3e70fc97311c6cd
suji-gelam <sujithagelam@gmail.com>
2026-07-25 11:46:14 +0530

    changed link

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://food-delivery-backend-5yum.onrender.com/";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
   const addToCart = async (itemId) => {
