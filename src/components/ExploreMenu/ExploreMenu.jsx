261eccdd95859d96f33ba02fc923615f1330a1df
suji-gelam <sujithagelam@gmail.com>
2026-07-26 14:37:48 +0530

    deleted explore mmenu

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -5,7 +5,6 @@ import { menu_list } from "../../assets/assets";
 const ExploreMenu = ({ category, setCategory }) => {
   return (
     <div className="explore-menu" id="explore-menu">
-      <h1>Explore our menu</h1>
       <p className="explore-menu-test">
         Choose from a diverse menu featuring a delectable array of dishes crated
         with the finest ingredents and culinary expertise.Our mission is to
